fe0bd563078fc15ebd1a7ed0926cbd8cbb60dd01
GollaLikhitha127 <likhithagolla055@gmail.com>
2026-05-22 16:49:44 +0530

    Update Day1_Create_Table.sql

--- Day1_Create_Table.sql
+++ Day1_Create_Table.sql
@@ -1,1 +1,9 @@
+CREATE DATABASE resturant_management;
+USE  resturant_management;
 
+CREATE TABLE restaurant(
+Restaurant_id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255),
+location VARCHAR(100),
+Rating DECIMAL(3,2)
+);
