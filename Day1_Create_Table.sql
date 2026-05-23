a2ed7c0cc4ceefad852eb0c185b61f501f349367
GollaLikhitha127 <likhithagolla055@gmail.com>
2026-05-23 13:28:13 +0530

    Create customer and orders tables with relationships

--- Day1_Create_Table.sql
+++ Day1_Create_Table.sql
@@ -7,3 +7,23 @@ name VARCHAR(255),
 location VARCHAR(100),
 Rating DECIMAL(3,2)
 );
+
+CREATE TABLE customer(
+CustomerID INT AUTO_INCREMENT PRIMARY KEY,
+FristName VARCHAR(49) NOT NULL,
+LastName VARCHAR(49) NOT NULL,
+Email VARCHAR(19) UNIQUE,
+Address VARCHAR(255)
+); 
+
+
+CREATE TABLE orders(
+OrderID INT AUTO_INCREMENT PRIMARY KEY,
+CustomerID INT,
+Restaurant_id INT,
+OrderDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+TotalAmount DECIMAL(10,3),
+Status VARCHAR(20),
+FOREIGN KEY (CustomerID) REFERENCES customer(CustomerID),
+FOREIGN KEY (Restaurant_id ) REFERENCES restaurant(Restaurant_id )
+);
